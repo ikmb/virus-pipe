7e941a200a2da0fbd870f7d4c3b07568789aec37
Marc Hoeppner <mphoeppner@gmail.com>
2022-04-19 09:31:53 +0200

    Fixing Pangolin parsing issue

--- bin/pangolin2yaml.pl
+++ bin/pangolin2yaml.pl
@@ -81,11 +81,11 @@ foreach my $file (glob("$dir/*.csv")) {
 		# taxon,lineage,conflict,ambiguity_score,scorpio_call,scorpio_support,scorpio_conflict,version,pangolin_version,pangoLEARN_version,pango_version,status,note
 		# EVEN NEWER: taxon,lineage,conflict,ambiguity_score,scoprio_call,scorpio_support,scorpio_notes,version,pangolin_version,scorpio_version,constellation_version,is_designated,qc_status,qc_notes,note
 
-                my ($seq,$lineage,$conflict,$ambig,$scorpio_call,$scorpio_support,$scorpio_notes,$scorpio_notes,$vers,$p_vers,$s_vers,$c_vers,$designated,$qc_status,$qc_notes,$note) = split(",", $line);
+                #my ($seq,$lineage,$conflict,$ambig,$scorpio_call,$scorpio_support,$scorpio_notes,$scorpio_notes,$vers,$p_vers,$s_vers,$c_vers,$designated,$qc_status,$qc_notes,$note) = split(",", $line);
 
-		# DEFUNCTmy ($seq,$lineage,$conflict,$ambig,$scorpio_call,$scorpio_support,$scorpio_conflict,$vers,$p_vers,$p_learn_vers,$p_vers,$status,$note) = split(",", $line);
+		my ($seq,$lineage,$conflict,$ambig,$scorpio_call,$scorpio_support,$scorpio_conflict,$vers,$p_vers,$p_learn_vers,$p_vers,$status,$note) = split(",", $line);
 
-		next unless ($qc_status eq "pass");
+		next unless ($status eq "passed_qc");
 		chomp($lineage);
 
 		my $trunk = (split /\./, $lineage)[0] ;
